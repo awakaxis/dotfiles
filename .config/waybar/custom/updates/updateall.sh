a95a993a35a62c1e73feacdc9901814ef2de0b48
awakaxis <awakaxis@gmail.com>
2025-05-31 05:54:52 -0700

    updates module now updates immediately after a system update
    thats a whole lotta update in one sentence
    it is 6 am i am so tired

--- .config/waybar/custom/updates/updateall.sh
+++ .config/waybar/custom/updates/updateall.sh
@@ -5,6 +5,7 @@ read "VAL?Do you want to update all packages? [Y/n]: "
 if [[ -z $VAL || $VAL == [Yy] ]]
 then
     sudo pacman -Syu
+    pkill -RTMIN+7 waybar # update counter immediately
     read -k1 "?Press any key to exit..."
 else
     exit
