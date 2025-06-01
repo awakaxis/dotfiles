861278fc0d684d7f56390832b1a93671e2f1077b
awakaxis <awakaxis@gmail.com>
2025-06-01 05:11:12 -0700

    added Open Config option to alpha
    lots of changes because this is the first time i've edited this file
    since getting a formatter

--- .config/nvim/lua/awakaxis/plugins/alpha.lua
+++ .config/nvim/lua/awakaxis/plugins/alpha.lua
@@ -1,34 +1,41 @@
 return {
-    "goolord/alpha-nvim",
-    even = "VimEnter",
-    config = function()
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.dashboard")
+	"goolord/alpha-nvim",
+	even = "VimEnter",
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-        dashboard.section.header.val = {
-            "                                                     ",
-            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-            "                                                     ",
-        }
+		dashboard.section.header.val = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		}
 
-        dashboard.section.buttons.val = {
-            dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-            dashboard.button("SPC ee", "  > Toggle file explorer", function() require("snacks.explorer").open() end),
-            dashboard.button("SPC ff", "󰱼  > Find File", function() require("snacks.picker").files() end),
-            dashboard.button("SPC fs", "  > Find Word", function() require("snacks.picker").grep() end),
-            dashboard.button("SPC wr", "󰁯  > Restore Session For CWD", "<cmd>SessionRestore<CR>"),
-            dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
-        }
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+			dashboard.button("SPC ee", "  > Toggle file explorer", function()
+				require("snacks.explorer").open()
+			end),
+			dashboard.button("SPC ff", "󰱼  > Find File", function()
+				require("snacks.picker").files()
+			end),
+			dashboard.button("SPC fs", "  > Find Word", function()
+				require("snacks.picker").grep()
+			end),
+			dashboard.button("c", "  > Open Config", ":cd ~/.config/nvim/<CR> | :e .<CR>"),
+			dashboard.button("SPC wr", "󰁯  > Restore Session For CWD", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+		}
 
-        -- Send config to alpha
-        alpha.setup(dashboard.opts)
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
 
-        -- Disable folding on alpha buffer
-        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-    end
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+	end,
 }
