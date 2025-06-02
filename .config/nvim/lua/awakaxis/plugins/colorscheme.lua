84102e6b1ce008c0752081124a95189fac251f4b
awakaxis <awakaxis@gmail.com>
2025-06-02 02:07:20 -0700

    fixed glorious gruvbox-material colors

--- .config/nvim/lua/awakaxis/plugins/colorscheme.lua
+++ .config/nvim/lua/awakaxis/plugins/colorscheme.lua
@@ -10,5 +10,16 @@ return {
 				transparent = true,
 			},
 		})
+
+		vim.api.nvim_set_hl(0, "@variable.python", { link = "GruvboxFg1" })
+		vim.api.nvim_set_hl(0, "@variable.member.python", { link = "GruvboxFg1" })
+		vim.api.nvim_set_hl(0, "@function.method.call.python", { link = "Green" })
+		vim.api.nvim_set_hl(0, "@constant.builtin.python", { link = "Red" })
+		vim.api.nvim_set_hl(0, "@constant.python", { link = "GruvboxFg1" })
+		vim.api.nvim_set_hl(0, "@variable.parameter.python", { link = "GruvboxFg1" })
+		vim.api.nvim_set_hl(0, "@type.python", { link = "Green" })
+		vim.api.nvim_set_hl(0, "@type.builtin.python", { link = "Red" })
+		vim.api.nvim_set_hl(0, "@operator.python", { link = "GruvboxFg1" })
+		vim.api.nvim_set_hl(0, "@module.python", { link = "GruvboxFg1" })
 	end,
 }
