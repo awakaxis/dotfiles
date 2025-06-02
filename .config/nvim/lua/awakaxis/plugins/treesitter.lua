9b8a05eac95331bd06e463ef41cee66e6feacc12
awakaxis <awakaxis@gmail.com>
2025-06-02 02:07:53 -0700

    minor changes
    that typo cost me two hours

--- .config/nvim/lua/awakaxis/plugins/treesitter.lua
+++ .config/nvim/lua/awakaxis/plugins/treesitter.lua
@@ -1,45 +1,51 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-        "windwp/nvim-ts-autotag"
-    },
-    config = function()
-        local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	-- event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+		"nvim-treesitter/playground",
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-        treesitter.setup({
-            hightlight = {
-                enable = true
-            },
-            indent = {
-                enable = true
-            },
-            autotag = {
-                enable = true
-            },
-            ensure_installed = {
-                "json",
-                "javascript",
-                "yaml",
-                "html",
-                "css",
-                "markdown",
-                "markdown_inline",
-                "bash",
-                "lua",
-                "vim",
-                "gitignore"
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = false,
-                    node_decremental = "<bs>"
-                }
-            }
-        })
-    end
+		treesitter.setup({
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"json",
+				"javascript",
+				"yaml",
+				"html",
+				"css",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"lua",
+				"vim",
+				"gitignore",
+				"python",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+			playground = {
+				enable = true,
+			},
+		})
+	end,
 }
