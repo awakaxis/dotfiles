8c5ad589d970191ba2c6590c2a28c1dc5c458158
awakaxis <awakaxis@gmail.com>
2025-06-27 07:09:43 -0700

    added install script it'll create symlinks based on the structure of the repo's files

---
+++ Scripts/install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/zsh
+
+echo "Looking for dotfiles to link out from the CWD ($(pwd))\nBackups will be created in $(pwd)/backups/"
+read -k1 "?press any key to continue..."
+
+for file in $(find .); do
+
+    if [[ $file == "./.git/"* || $file == "./backups"* || $file == "./Scripts/install.sh" ]]; then
+        echo "skipping: $file"
+        continue
+    fi
+
+    BACKUPS="$(pwd)/backups/"
+
+    if [[ ! -d $BACKUPS ]]; then
+        mkdir $BACKUPS
+    fi
+
+    realfile="$(pwd)${file:1}"
+    dest="${HOME}${file:1}"
+    # echo "dest: ${dest}"
+
+    if [ -d $realfile ]; then
+        if [ ! -d $dest ]; then
+            echo "Creating  ${dest}  because it does not exist"
+            mkdir $dest
+        fi
+    else
+       if [ -f $dest ]; then
+           echo "File exists: $dest"
+           read "VAL?Do you want to replace this file? [Y/n]: "
+           if [[ -z $VAL || $VAL == [Yy] ]]; then
+               echo "Replacing..."
+               bakfile="$BACKUPS${dest/${HOME}\//}.bak"
+               mkdir -p $(dirname $bakfile)
+               cp $dest $bakfile
+
+               rm $dest
+               ln -s $realfile $dest
+               echo "Replaced $dest."
+           fi
+       else
+           echo "Creating link for: $dest"
+           ln -s $realfile $dest
+       fi
+    fi
+done
+
