acf3a8b6212b2c2795496e14dbd32bccfc26a5ea
awakaxis <awakaxis@gmail.com>
2025-06-27 01:24:28 -0700

    added utility functions

--- .config/nvim/lua/awakaxis/main/options.lua
+++ .config/nvim/lua/awakaxis/main/options.lua
@@ -42,3 +42,32 @@ opt.isfname:append("@-@")
 opt.updatetime = 50
 
 opt.colorcolumn = "80"
+
+vim.api.nvim_create_user_command("Redir", function(ctx)
+	-- vim.cmd("redir => g:__capture_output | silent ! " .. ctx.args .. " | redir END")
+	vim.cmd("redir => g:__capture_output")
+	vim.cmd("silent! " .. ctx.args)
+	vim.cmd("redir END")
+	local lines = vim.split(vim.g.__capture_output or "", "\n")
+	vim.cmd("new")
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+	vim.opt_local.modified = false
+end, { nargs = "+", complete = "command" })
+
+function _G.dump_windows()
+	local table = {}
+	for i, window in ipairs(vim.api.nvim_list_wins()) do
+		local buffer = vim.api.nvim_win_get_buf(window)
+		local buffer_name = vim.api.nvim_buf_get_name(buffer)
+		local buffer_ft = vim.api.nvim_get_option_value("filetype", { buf = buffer })
+		local buffer_type = vim.api.nvim_get_option_value("buftype", { buf = buffer })
+
+		table[window] = {
+			name = buffer_name,
+			ft = buffer_ft,
+			type = buffer_type,
+		}
+	end
+	print(vim.inspect(table))
+	return table
+end
