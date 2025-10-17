6ac58d761f495aa37e8b57b1d1502a25be37edf9
awakaxis <awakaxis@gmail.com>
2025-10-17 07:53:34 -0700

    fix: auto-session works now

--- .config/nvim/lua/awakaxis/plugins/auto-session.lua
+++ .config/nvim/lua/awakaxis/plugins/auto-session.lua
@@ -5,19 +5,16 @@ return {
 
 		auto_session.setup({
 			auto_restore_enabled = false,
-			auto_session_supres_dirs = { "~/", "~/Dev/", "~/Downloads/", "~/Documents/", "~/Desktop/" },
+			auto_session_supres_dirs = {},
 		})
 
 		local map = vim.keymap
 
 		map.set("n", "<leader>wr", function()
-			for i, win in ipairs(dump_windows()) do
-				if win.ft == "snacks_picker_list" then
-					require("snacks.explorer").open()
-				end
-			end
 			vim.cmd.AutoSession("restore")
 		end, { desc = "Restore session for cwd" })
-		map.set("n", "<leader>ws", vim.cmd.AutoSession("save"), { desc = "Save session for auto session root dir" })
+		map.set("n", "<leader>ws", function()
+			vim.cmd.AutoSession("save")
+		end, { desc = "Save session for auto session root dir" })
 	end,
 }
